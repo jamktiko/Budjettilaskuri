e76fa1d21cac3a3202abe63d70674ec7e60f30b8
Aapo Hampaala <ag3652@student.jamk.fi>
2026-04-27 15:28:27 +0300

    uudestaan alustus, ei mennyt ensin läpi

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,32 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
+
+const mongoose = require('mongoose');
+require('dotenv').config(); // Jos käytät .env tiedostoa paikallisesti
+
+// Käytetään Beanstalkin ympäristömuuttujaa tai paikallista testijonoa
+const mongoURI = process.env.MONGO_URI;
+
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log('✅ MongoDB Atlas yhteys muodostettu!'))
+  .catch((err) => console.error('❌ MongoDB yhteysvirhe:', err));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use('/api', require('./routes/test'));
+const cors = require('cors');
+app.use(cors()); // Sallii kutsut mistä tahansa (kehitysvaiheessa ok)
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -23,12 +38,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
