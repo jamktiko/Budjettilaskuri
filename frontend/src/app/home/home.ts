c128b6e39d6068f75019fd975bf5bb2a48734e70
teemu, jalava <ag2353@student.jamk.fi>
2026-04-27 15:25:06 +0300

    homekomponenttiin / kotivalikkoon lisatty saldo, tulot, menot nakymat

--- frontend/src/app/home/home.ts
+++ frontend/src/app/home/home.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BudgetService } from '../shared/budget.service';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrls: ['./home.css'],
 })
 export class Home {
-
+  constructor(public budget: BudgetService) {}
 }
