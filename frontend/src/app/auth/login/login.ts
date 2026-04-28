6001b92f42c590173799569d01642c2e58139aae
Aapo Hampaala <ag3652@student.jamk.fi>
2026-04-28 10:25:56 +0300

    session korjattu

--- frontend/src/app/auth/login/login.ts
+++ frontend/src/app/auth/login/login.ts
@@ -3,12 +3,13 @@ import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { signIn } from 'aws-amplify/auth'; // <-- Tuodaan Amplifyn signIn-funktio
 import { AmplifyAuthenticatorModule, AuthenticatorService } from '@aws-amplify/ui-angular';
+import { OnInit } from '@angular/core';
 
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { Subscription } from 'rxjs/internal/Subscription';
+import { Subscription } from 'rxjs';
 import { I18n } from 'aws-amplify/utils';
 @Component({
   selector: 'app-login',
@@ -25,12 +26,9 @@ import { I18n } from 'aws-amplify/utils';
   styleUrls: ['./login.css'],
 })
 export class Login {
-  identifier = ''; // Muutettu email -> identifier
-  password = '';
   private authSubscription?: any;
-
   constructor(
-    public authenticator: AuthenticatorService, // <-- Injektoi AmplifyAuthenticator
+    public authenticator: AuthenticatorService,
     private router: Router,
   ) {}
   ngOnInit() {
@@ -51,34 +49,12 @@ export class Login {
     });
 
     I18n.setLanguage('fi');
-  }
-
-  ngOnDestroy() {
-    // Muista perua tilaus, kun komponentti poistuu, jotta ei tule muistivuotoja
-    this.authSubscription?.unsubscribe();
-  }
-  // Muutettu async-funktioksi, koska verkkopyyntö vie aikaa
-  async login() {
-    try {
-      // Lähetetään tiedot Cognitolle
-      const { isSignedIn, nextStep } = await signIn({
-        username: this.identifier, // Amplify käyttää termiä 'username', vaikka antaisit sähköpostin
-        password: this.password,
-      });
-
-      if (isSignedIn) {
-        console.log('Kirjautuminen onnistui Cognitosta!');
-        // Siirtyy home-sivulle vasta kun Cognito on näyttänyt vihreää valoa
+    this.authSubscription = this.authenticator.subscribe((data) => {
+      if (data.authStatus === 'authenticated') {
+        console.log('Käyttäjä on jo sisällä, ohjataan kotiin...');
         this.router.navigate(['/home']);
-      } else {
-        // Tämä tapahtuu, jos Cognito vaatii esim. uuden salasanan asettamista
-        console.log('Kirjautuminen vaatii lisäaskeleita:', nextStep);
       }
-    } catch (error) {
-      console.error('Virhe kirjautumisessa', error);
-      // Yksinkertainen virheilmoitus käyttäjälle (voit myöhemmin vaihtaa esim. MatSnackBar-komponenttiin)
-      alert('Väärä tunnus, sähköposti tai salasana!');
-    }
+    });
   }
 
   public formFields = {
