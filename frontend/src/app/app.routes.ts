7ee6da810a20872578309e91bbea412eadec3523
teemu, jalava <ag2353@student.jamk.fi>
2026-04-28 09:55:40 +0300

    lisatty home lehdelle saldo, tulot, menot seka nav, korjattu yhteensopivuudet

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Login } from './auth/login/login';
 import { Home } from './home/home';
 import { Register } from './register/register';
 import { authGuard } from './auth.guard';
+import { AddExpense } from './expense/add-expense/add-expense';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
--- frontend/src/app/home/home.ts
+++ frontend/src/app/home/home.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { AuthenticatorService } from '@aws-amplify/ui-angular';
 import { Router } from '@angular/router';
+import { BudgetService } from '../shared/budget.service';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -12,6 +15,7 @@ export class Home {
   constructor(
     public authenticator: AuthenticatorService,
     private router: Router,
+    public budget: BudgetService,
   ) {}
 
   // Voit halutessasi tehdä oman metodin uloskirjautumiselle
