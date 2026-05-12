9929bce1c94268920057d6c7a2b059d8ce437cf1
Aapo Hampaala <ag3652@student.jamk.fi>
2026-05-12 13:49:45 +0300

    markasread poista

--- frontend/src/app/shared/notification/notification.service.ts
+++ frontend/src/app/shared/notification/notification.service.ts
@@ -10,7 +10,7 @@ export class NotificationService {
   private notificationsSubject = new BehaviorSubject<AppNotification[]>([]);
 
   notifications$ = this.notificationsSubject.asObservable();
-  unreadCount$ = this.notifications$.pipe(map((list) => list.filter((n) => !n.read).length));
+  unreadCount$ = this.notifications$.pipe(map((list) => list.length));
 
   // Poista latest$ ja luo tilalle uusi Subject pop-upeille
   private toastSubject = new Subject<AppNotification>();
@@ -34,16 +34,6 @@ export class NotificationService {
 
     // Lähetetään tieto Snackbarille vain kun uusi ilmoitus OIKEASTI lisätään
     this.toastSubject.next(notification);
-  }
-
-  markAsRead(id: string): void {
-    const updated = this.notificationsSubject.value.map((n) =>
-      n.id === id ? { ...n, read: true } : n,
-    );
-
-    this.notificationsSubject.next(updated);
-
-    this.saveToLocalStorage();
   }
 
   dismiss(id: string): void {
